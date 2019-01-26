ed74bf896a8b8083d4900d31f91d60d81b7db977
Elise Lee <eliselee2021@u.northwestern.edu>
2019-01-25 21:08:26 -0600

    Design profile screen layout

--- yekyok/screens/ProfileScreen.js
+++ yekyok/screens/ProfileScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { Platform, Text, View, ScrollView, StyleSheet } from 'react-native';
+import { Icon } from 'expo';
 
 export default class ProfileScreen extends React.Component {
   static navigationOptions = {
@@ -16,6 +17,26 @@ export default class ProfileScreen extends React.Component {
   render() {
     return (
       <ScrollView style={styles.container}>
+      <View style={styles.header}>
+        <Icon.Ionicons
+          name={Platform.OS === 'ios' ? 'ios-contact' : 'md-contact'}
+          size={110}
+          style={styles.profileIcon}/>
+          <View style={{flexDirection:"column"}}>
+            <Text style={styles.userName}>willie wildcat</Text>
+            <Text style={styles.userDate}>joined: october 18th, 2018</Text>
+          </View>
+        </View>
+      <Text style={styles.headings}>starred events</Text>
+      <View style={styles.divider}/>
+      <View style={styles.greyBar}></View>
+      <View style={styles.greyBar}></View>
+      <Text style={styles.headings}>past events</Text>
+      <View style={styles.divider}/>
+      <View style={styles.greyBar}></View>
+      <View style={styles.greyBar}></View>
+      <View style={styles.greyBar}></View>
+      <View style={styles.greyBar}></View>
       </ScrollView>
     );
   }
@@ -26,6 +47,49 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 15,
     backgroundColor: '#fff',
+  },
+  header: {
+    flexDirection:"row",
+    backgroundColor: '#dec9ff',
+    marginBottom: 20,
+    height: 130,
+  },
+  greyBar: {
+    flexDirection:"row",
+    backgroundColor: '#ededed',
+    marginBottom: 20,
+    height: 120,
+  },
+  userDate: {
+    paddingLeft: 25,
+    fontSize: 18,
+    fontStyle: 'italic',
+    paddingRight: 30,
+    flexWrap: 'wrap',
+  },
+  userName: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    paddingTop: 30,
+    paddingLeft: 15,
+    paddingRight: 30,
+    flexWrap: 'wrap',
+  },
+  headings: {
+    fontSize: 50,
+    paddingLeft: 30,
+    color: '#c1c1c1',
+  },
+  divider: {
+    borderTopColor: '#D3D3D3',
+    borderTopWidth: 1,
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+  profileIcon: {
+    marginTop: 10,
+    marginLeft: 30,
   }
 });
 
