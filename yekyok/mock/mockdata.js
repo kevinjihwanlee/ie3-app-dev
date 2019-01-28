fe281be013f3aef218e493223a6bdb0bcb1b8f87
Kevin J Lee <kevin.jihwan.lee@gmail.com>
2019-01-27 21:38:15 -0600

    added react native elements dependency, mock data

---
+++ yekyok/mock/mockdata.js
@@ -0,0 +1,25 @@
+// mock data
+// if you think each event object needs more attributes, include them in your PR
+
+let events = [
+  {
+    id: '1',
+    name: 'Norris Grand Opening',
+    author: 'id123',
+    description: 'Come celebrate the grand opening of Norris with free food!',
+    date_created: Date.now(),
+    saved: 2,
+    latitude: 42.05336,
+    longitude: -87.672662,
+  },
+  {
+    id: '2',
+    name: 'a party',
+    author: 'id321',
+    description: 'party to end all parties at ridge and noyes, you cannot miss this spectacle',
+    date_created: Date.now(),
+    saved: 45,
+    latitude: 42.05336,
+    longitude: -87.672662,
+  },
+]
